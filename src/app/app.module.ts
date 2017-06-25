b92de038d308373eeb668cffc656f4c869bcc90f
spycebot <photius@shannonware.com>
2017-06-25 13:05:58 +0100

    Select fonts and implement app-wide.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } 	from '@angular/platform-browser';
-import { NgModule } 		from '@angular/core';
+import { NgModule } 		  from '@angular/core';
 import { FormsModule }		from '@angular/forms';
+import { RouterModule, Routes }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PlayerDetailComponent } from './player-detail.component'
 import { ShipDetailComponent } from './ship-detail.component';
 import { PortDetailComponent } from './port-detail.component';
+
+/*
+const routes: Routes = [
+  { path: '', }
+];
+*/
 
 @NgModule({
   declarations: [
@@ -16,7 +23,13 @@ import { PortDetailComponent } from './port-detail.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule /*,
+    RouterModule.forRoot([
+        {
+          path:'port',
+          component: PortDetailComponent
+        }
+    ]) */
   ],
   providers: [],
   bootstrap: [AppComponent]
